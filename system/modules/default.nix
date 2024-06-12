8939f8db6b70661377df6d249ab04f65444d4caa
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2024-06-12 18:50:23 +0700

    fix: missing openssh import

--- system/modules/default.nix
+++ system/modules/default.nix
@@ -4,19 +4,20 @@
     ./android.nix
     ./audio.nix
     ./avahi.nix
-    ./brightnessctl.nix
-    ./flatpak.nix
-    ./gnome.nix
-    ./kde.nix
     ./bluetooth.nix
     ./boot_loader.nix
+    ./brightnessctl.nix
     ./docker.nix
-    ./sops.nix
+    ./flatpak.nix
     ./font.nix
+    ./gnome.nix
     ./hyprland.nix
+    ./kde.nix
     ./networking.nix
+    ./openssh.nix
     ./printing.nix
     ./scanner.nix
+    ./sops.nix
     ./steam.nix
     ./sudo.nix
     ./vial.nix
