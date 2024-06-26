92cb15265ce535a4c1ee9a5f084e3c179b12da47
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2024-06-26 13:26:32 +0700

    sonarr: added anime instance

--- system/podman/servarr/sonarr.nix
+++ system/podman/servarr/sonarr.nix
@@ -3,12 +3,16 @@ let
   podman = config.profile.podman;
   sonarr = podman.servarr.sonarr;
   name = "sonarr";
+  name-anime = "${name}-anime";
   ip = "10.88.2.3";
+  ip-anime = "10.88.2.33";
   image = "lscr.io/linuxserver/sonarr:latest";
   root = "/nas/mediaserver/servarr";
-  configVolume = "${root}/sonarr";
+  configVolume = "${root}/${name}";
+  configVolumeAnime = "${root}/${name-anime}";
   mediaVolume = "${root}/data";
   domain = "${name}.tigor.web.id";
+  domain-anime = "${name-anime}.tigor.web.id";
   user = config.profile.user;
   uid = toString user.uid;
   gid = toString user.gid;
@@ -20,9 +24,13 @@ in
       reverse_proxy ${ip}:8989
     '';
 
+    services.caddy.virtualHosts.${domain-anime}.extraConfig = ''
+      reverse_proxy ${ip-anime}:8989
+    '';
+
     system.activationScripts."podman-${name}" = ''
-      mkdir -p ${configVolume} ${mediaVolume}
-      chown ${uid}:${gid} ${mediaVolume} ${configVolume}
+      mkdir -p ${configVolume} ${mediaVolume} ${configVolumeAnime}
+      chown ${uid}:${gid} ${mediaVolume} ${configVolume} ${configVolumeAnime}
     '';
 
     virtualisation.oci-containers.containers.${name} = {
@@ -40,6 +48,28 @@ in
       ];
       extraOptions = [
         "--ip=${ip}"
+        "--network=podman"
+      ];
+      labels = {
+        "io.containers.autoupdate" = "registry";
+      };
+    };
+
+    virtualisation.oci-containers.containers.${name-anime} = {
+      inherit image;
+      hostname = name-anime;
+      autoStart = true;
+      environment = {
+        PUID = uid;
+        PGID = gid;
+        TZ = "Asia/Jakarta";
+      };
+      volumes = [
+        "${configVolumeAnime}:/config"
+        "${mediaVolume}:/data"
+      ];
+      extraOptions = [
+        "--ip=${ip-anime}"
         "--network=podman"
       ];
       labels = {
