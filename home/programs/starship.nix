fe870e078517778cbd3e4279d649eae63bc49332
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2024-07-10 10:58:09 +0700

    starship: refactor for more readable code

--- home/programs/starship.nix
+++ home/programs/starship.nix
@@ -3,6 +3,12 @@
   programs.starship =
     let
       flavour = "mocha";
+      catppuccin-starship-repo = pkgs.fetchFromGitHub {
+        owner = "catppuccin";
+        repo = "starship";
+        rev = "5629d2356f62a9f2f8efad3ff37476c19969bd4f"; # Replace with the latest commit hash
+        sha256 = "sha256-nsRuxQFKbQkyEI4TXgvAjcroVdG+heKX5Pauq/4Ota0=";
+      };
     in
     {
       enable = true;
@@ -30,12 +36,6 @@
         };
         palette = "catppuccin_${flavour}";
       } // builtins.fromTOML (builtins.readFile
-        (pkgs.fetchFromGitHub
-          {
-            owner = "catppuccin";
-            repo = "starship";
-            rev = "5629d2356f62a9f2f8efad3ff37476c19969bd4f"; # Replace with the latest commit hash
-            sha256 = "sha256-nsRuxQFKbQkyEI4TXgvAjcroVdG+heKX5Pauq/4Ota0=";
-          } + /palettes/${flavour}.toml));
+        (catppuccin-starship-repo + /palettes/${flavour}.toml));
     };
 }
