9a1a807c5212c58aec4fc57471b15be49272564f
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2024-07-26 00:15:55 +0700

    zellij: added zstatus plugin

--- home/programs/zellij.nix
+++ home/programs/zellij.nix
@@ -1,4 +1,4 @@
-{ config, lib, ... }:
+{ config, lib, pkgs, ... }:
 let
   cfg = config.profile.home.programs.zellij;
 in
@@ -179,5 +179,51 @@ in
         }
       }
     '';
+
+    home.file.".config/zellij/layouts/default.kdl".text =
+      let
+        version = "0.17.0";
+        zstatus = pkgs.fetchurl {
+          url = "https://github.com/dj95/zjstatus/releases/download/v${version}/zjstatus.wasm";
+          hash = "sha256-IgTfSl24Eap+0zhfiwTvmdVy/dryPxfEF7LhVNVXe+U";
+        };
+      in
+        /*kdl*/ ''
+        layout {
+            default_tab_template {
+                children
+                pane size=1 borderless=true {
+                    plugin location="file:${zstatus}" {
+                        format_left   "{mode} #[fg=#89B4FA,bold]{session}"
+                        format_center "{tabs}"
+                        format_right  "{command_git_branch} {datetime}"
+                        format_space  ""
+
+                        border_enabled  "false"
+                        border_char     "─"
+                        border_format   "#[fg=#6C7086]{char}"
+                        border_position "top"
+
+                        hide_frame_for_single_pane "true"
+
+                        mode_normal  "#[bg=blue] "
+                        mode_tmux    "#[bg=#ffc387] "
+
+                        tab_normal   "#[fg=#6C7086] {name} "
+                        tab_active   "#[fg=#9399B2,bold,italic] {name} "
+
+                        command_git_branch_command     "git rev-parse --abbrev-ref HEAD"
+                        command_git_branch_format      "#[fg=blue] {stdout} "
+                        command_git_branch_interval    "10"
+                        command_git_branch_rendermode  "static"
+
+                        datetime        "#[fg=#6C7086,bold] {format} "
+                        datetime_format "%A, %d %b %Y %H:%M"
+                        datetime_timezone "Europe/Berlin"
+                    }
+                }
+            }
+        }
+      '';
   };
 }
