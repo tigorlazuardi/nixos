557e26bae5b203de740f0e4b700cb2027bb60ca3
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2024-07-26 16:57:20 +0700

    foot: added config (not installed yet)

---
+++ home/programs/foot.nix
@@ -0,0 +1,20 @@
+{ config, lib, ... }:
+let
+  cfg = config.profile.home.programs.foot;
+in
+{
+  config = lib.mkIf cfg.enable {
+    programs.foot = {
+      enable = true;
+      settings = {
+        main = {
+          font = "JetBrainsMono Nerd Font Mono:size=12";
+          include = lib.mkIf config.profile.hyprland.enable "${config.home.homeDirectory}/.config/foot/colors.ini";
+        };
+        mouse = {
+          hide-when-typing = "yes";
+        };
+      };
+    };
+  };
+}
