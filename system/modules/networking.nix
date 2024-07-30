c7b156c084d450ebd83220dff34ddb27c60747b9
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2024-07-30 16:07:55 +0700

    bareksa: domain changed from tools.bareksa.local -> apicurio.prod.bareksa.local

--- system/modules/networking.nix
+++ system/modules/networking.nix
@@ -3,7 +3,7 @@
   networking.networkmanager.enable = true;
   networking.extraHosts = ''
     192.168.50.217 gitlab.bareksa.com
-    192.168.50.205 tools.bareksa.local
+    192.168.50.205 apicurio.prod.bareksa.local
     192.168.3.50 kafka.dev.bareksa.local
     192.168.3.109 redpanda.dev.bareksa.local kafka-console.dev.bareksa.local
   '';
