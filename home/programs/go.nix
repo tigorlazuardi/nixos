186711342c195bfa584c27b710368e7122345278
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2024-08-12 21:18:22 +0700

    go: fix GOROOT and GOPATH and add toolings

--- home/programs/go.nix
+++ home/programs/go.nix
@@ -1,6 +1,17 @@
 { config, lib, pkgs, ... }:
 let
   cfg = config.profile.go;
+  impl = pkgs.buildGoModule rec {
+    pname = "impl";
+    version = "1.4.0";
+    src = pkgs.fetchFromGitHub {
+      owner = "josharian";
+      repo = "impl";
+      rev = "v${version}";
+      sha256 = "sha256-0TSyg7YEPur+h0tkDxI3twr2PzT7tmo3shKgmSSJ6qk=";
+    };
+    vendorHash = "sha256-vTqDoM/LK5SHkayLKYig+tCrXLelOoILmQGCxlTWHog=";
+  };
 in
 {
   config = lib.mkIf cfg.enable {
@@ -17,6 +28,15 @@ in
       gomodifytags
       gotests
       iferr
+      gopls
+      gofumpt
+      impl
     ];
+
+    # Some toolings will lookup for $GOROOT env.
+    home.sessionVariables = {
+      GOROOT = "${pkgs.go}/share/go";
+      GOPATH = "${config.home.homeDirectory}/go";
+    };
   };
 }
