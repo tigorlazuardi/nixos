380b8dd681b5ed64cb1185e7ca8e598902974ffe
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2024-08-27 15:24:20 +0700

    bareksa: added kafka prod ip

--- system/modules/networking.nix
+++ system/modules/networking.nix
@@ -6,6 +6,9 @@
     192.168.50.205 apicurio.prod.bareksa.local
     192.168.3.50 kafka.dev.bareksa.local
     192.168.3.109 redpanda.dev.bareksa.local kafka-console.dev.bareksa.local
+    192.168.50.102 kafka1.prod.bareksa.local
+    192.168.50.103 kafka2.prod.bareksa.local
+    192.168.50.104 kafka3.prod.bareksa.local
   '';
   networking.firewall =
     let
