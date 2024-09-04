54db18893e57a96e0a68dafc5a1b014d5f411282
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2024-09-04 12:35:10 +0700

    neovim: use unstable instead of nightly to fix golang treesitter

--- system/programs.nix
+++ system/programs.nix
@@ -1,11 +1,12 @@
-{ inputs, pkgs, ... }:
+{ inputs, pkgs, unstable, ... }:
 {
   programs.neovim = {
     enable = true;
     defaultEditor = true;
     viAlias = true;
     vimAlias = true;
-    package = inputs.neovim-nightly-overlay.packages.${pkgs.system}.default;
+    # package = inputs.neovim-nightly-overlay.packages.${pkgs.system}.default;
+    package = unstable.neovim-unwrapped;
   };
 
   environment.systemPackages = with pkgs; [
