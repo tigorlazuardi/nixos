ae5b09d7ab667188b53344c16cf3c929ee4f3a14
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2024-09-05 16:22:50 +0700

    hyprland: removed zen browser because there's always 30 second delay on start

--- home/modules/hyprland/hyprland.nix
+++ home/modules/hyprland/hyprland.nix
@@ -91,12 +91,7 @@ in
           # Programs
           ''$mod, RETURN, exec, footclient''
           "$mod, E, exec, thunar"
-          (if config.profile.flatpak.zen-browser.enable
-          then
-            "$mod, B, exec, flatpak run io.github.zen_browser.zen"
-          else
-            "$mod, B, exec, microsoft-edge"
-          )
+          "$mod, B, exec, microsoft-edge"
           "$mod, D, exec, rofi -show drun -replace -i"
           "$mod, BackSpace, exec, wlogout"
           ''$mod, F, exec, ${select-window.path}''
