62a047ce7769c55e0da7b64e4a20e6c6aef5f8fe
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2024-10-17 17:31:12 +0700

    swaync: style uses catppuccin mocha theme

--- home/modules/hyprland/sway-notification-center.nix
+++ home/modules/hyprland/sway-notification-center.nix
@@ -12,6 +12,13 @@ in
     home.file.".config/swaync/config.json".text = builtins.toJSON {
       positionX = "center";
       positionY = "top";
+      fit-to-screen = false;
+      control-center-height = 800;
+    };
+
+    home.file.".config/swaync/style.css".source = pkgs.fetchurl {
+      url = "https://github.com/catppuccin/swaync/releases/download/v0.2.3/mocha.css";
+      hash = "sha256-Hie/vDt15nGCy4XWERGy1tUIecROw17GOoasT97kIfc=";
     };
 
     wayland.windowManager.hyprland.settings.exec-once = [
