a0781b380d754fbaea217867b9df35223a65b855
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2024-10-17 18:08:40 +0700

    swaync: added notification sound effect

--- home/modules/hyprland/sway-notification-center.nix
+++ home/modules/hyprland/sway-notification-center.nix
@@ -9,11 +9,22 @@ in
       libnotify
     ];
 
+    # Config: https://manpages.debian.org/testing/sway-notification-center/swaync.5.en.html
     home.file.".config/swaync/config.json".text = builtins.toJSON {
       positionX = "center";
       positionY = "top";
       fit-to-screen = false;
       control-center-height = 800;
+      timeout = 5;
+      timeout-low = 3;
+
+      scripts = {
+        play-notification-sound = {
+          exec = ''${pkgs.sox}/bin/play --volume 0.5 ${./gran_turismo_menu_sound_effect.mp3}'';
+          app-name = ".*";
+          urgency = "Normal";
+        };
+      };
     };
 
     home.file.".config/swaync/style.css".source = pkgs.fetchurl {
