d7799ab8ad8b1c0aa76cef654308f6114c83677d
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2024-10-20 15:40:28 +0700

    swaync: uses home manager module

--- home/modules/hyprland/sway-notification-center.nix
+++ home/modules/hyprland/sway-notification-center.nix
@@ -4,36 +4,32 @@ let
 in
 {
   config = lib.mkIf cfg.enable {
-    home.packages = with pkgs; [
-      swaynotificationcenter
-      libnotify
-    ];
+    services.swaync = {
+      enable = true;
+      style = pkgs.fetchurl {
+        url = "https://github.com/catppuccin/swaync/releases/download/v0.2.3/mocha.css";
+        hash = "sha256-Hie/vDt15nGCy4XWERGy1tUIecROw17GOoasT97kIfc=";
+      };
+      settings = {
+        positionX = "center";
+        positionY = "top";
+        fit-to-screen = false;
+        control-center-height = 800;
+        timeout = 5;
+        timeout-low = 3;
 
-    # Config: https://manpages.debian.org/testing/sway-notification-center/swaync.5.en.html
-    home.file.".config/swaync/config.json".text = builtins.toJSON {
-      positionX = "center";
-      positionY = "top";
-      fit-to-screen = false;
-      control-center-height = 800;
-      timeout = 5;
-      timeout-low = 3;
-
-      scripts = {
-        play-notification-sound = {
-          exec = ''${pkgs.sox}/bin/play --volume 0.5 ${./gran_turismo_menu_sound_effect.mp3}'';
-          app-name = "^(?!discord|TelegramDesktop|Slack|Signal|Element).*$";
-          urgency = "Normal";
+        scripts = {
+          _99-play-notification-sound = {
+            exec = ''${pkgs.sox}/bin/play --volume 0.5 ${./gran_turismo_menu_sound_effect.mp3}'';
+            app-name = "^(?!discord|TelegramDesktop|Slack|slack|Signal|Element).*$";
+            urgency = "Normal";
+          };
         };
       };
     };
 
-    home.file.".config/swaync/style.css".source = pkgs.fetchurl {
-      url = "https://github.com/catppuccin/swaync/releases/download/v0.2.3/mocha.css";
-      hash = "sha256-Hie/vDt15nGCy4XWERGy1tUIecROw17GOoasT97kIfc=";
-    };
-
-    wayland.windowManager.hyprland.settings.exec-once = [
-      "swaync"
+    home.packages = with pkgs; [
+      libnotify
     ];
   };
 }
