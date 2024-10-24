589e1851700f405a3869d0a02e4d2434a931fc1e
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2024-10-24 17:27:53 +0700

    microsoft-edge: use stable instead of unstable

--- home/programs/microsoft-edge.nix
+++ home/programs/microsoft-edge.nix
@@ -1,12 +1,12 @@
 {
   config,
   lib,
-  unstable,
+  pkgs,
   ...
 }:
 let
   cfg = config.profile.microsoft-edge;
 in
 {
-  config = lib.mkIf cfg.enable { home.packages = [ unstable.microsoft-edge ]; };
+  config = lib.mkIf cfg.enable { home.packages = [ pkgs.microsoft-edge ]; };
 }
