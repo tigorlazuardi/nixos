8cbde4811fc630acfaae3c9b9b1983f206dc6342
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2024-10-28 15:00:25 +0700

    bareksa: removed kafka and redpanda from networking module. Also removed 192.168.100.0 IP range from openvpn config

--- system/modules/networking.nix
+++ system/modules/networking.nix
@@ -3,12 +3,6 @@
   networking.networkmanager.enable = true;
   networking.extraHosts = ''
     192.168.50.217 gitlab.bareksa.com
-    192.168.50.205 apicurio.prod.bareksa.local
-    192.168.3.50 kafka.dev.bareksa.local
-    192.168.3.109 redpanda.dev.bareksa.local kafka-console.dev.bareksa.local
-    192.168.50.102 kafka1.prod.bareksa.local
-    192.168.50.103 kafka2.prod.bareksa.local
-    192.168.50.104 kafka3.prod.bareksa.local
   '';
   networking.firewall =
     let
