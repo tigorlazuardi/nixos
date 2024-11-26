620d15277d65c3d797b56dba224ac7723fc5e97c
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2024-11-26 09:58:01 +0700

    castle: added clr.icd to the graphics extraPackages

--- hardware-configuration/castle.nix
+++ hardware-configuration/castle.nix
@@ -28,7 +28,10 @@
   hardware.graphics = {
     enable = true;
     enable32Bit = true;
-    extraPackages = with pkgs; [ amdvlk ];
+    extraPackages = with pkgs; [
+      amdvlk
+      rocmPackages.clr.icd
+    ];
   };
 
   environment.systemPackages = with pkgs; [ lact ];
