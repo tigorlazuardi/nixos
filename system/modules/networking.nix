77d8954f557734c748c8651d59f68aa47196b877
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2024-11-28 13:25:22 +0700

    added back extra host

--- system/modules/networking.nix
+++ system/modules/networking.nix
@@ -9,6 +9,7 @@
     enable = true;
     appendNameservers = [ ] ++ lib.lists.optional config.services.adguardhome.enable "192.168.100.5";
   };
+  networking.extraHosts = "192.168.50.217 gitlab.bareksa.com";
   networking.firewall =
     let
       cfg = config.profile.networking.firewall;
