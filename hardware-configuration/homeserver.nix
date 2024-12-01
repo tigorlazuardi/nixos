a5ab4fb9e58adeb69909a9b1fe63854f4a74ec11
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2024-12-01 12:01:50 +0700

    homeserver: added more vaapi drivers and OpenCL support

--- hardware-configuration/homeserver.nix
+++ hardware-configuration/homeserver.nix
@@ -171,9 +171,13 @@
       enable = true;
       enable32Bit = true;
       extraPackages = with pkgs; [
-        intel-media-driver
-        intel-vaapi-driver
         libvdpau-va-gl
+        intel-media-driver
+        intel-vaapi-driver # previously vaapiIntel
+        vaapiVdpau
+        intel-compute-runtime # OpenCL filter support (hardware tonemapping and subtitle burn-in)
+        vpl-gpu-rt # QSV on 11th gen or newer
+        intel-media-sdk # QSV up to 11th gen
       ];
     };
     environment.sessionVariables = {
