128bd55e8f0faf6c1edf968a600f491b36989b07
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2025-02-02 14:07:20 +0700

    ollama: only enabled if more than models are registered

--- system/services/ollama.nix
+++ system/services/ollama.nix
@@ -1,5 +1,6 @@
 {
   config,
+  lib,
   ...
 }:
 let
@@ -7,14 +8,18 @@ let
   enable = (builtins.length cfg.models) > 0;
 in
 {
-  services.ollama = {
-    enable = enable;
-  };
-
-  services.open-webui = {
-    enable = enable;
-    environment = {
-      OLLAMA_API_BASE_URL = "http://127.0.0.1:11434";
+  config = lib.mkIf enable {
+    services.ollama = {
+      enable = enable;
     };
+
+    services.open-webui = {
+      enable = enable;
+      environment = {
+        OLLAMA_API_BASE_URL = "http://127.0.0.1:11434";
+      };
+    };
+
+    environment.variables.OLLAMA_API_BASE_URL = "http://127.0.0.1:11434";
   };
 }
