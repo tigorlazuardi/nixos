284137b7e5650acdfbceeac807de73c8a1282100
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2025-02-18 13:20:27 +0700

    nixvim: added neovide transparency

--- nixvim/default.nix
+++ nixvim/default.nix
@@ -6,7 +6,10 @@
   # Dependencies are not defined here, but whoever imports this module.
   programs.nixvim = {
     enable = true;
-    colorschemes.catppuccin.enable = true;
+    colorschemes.catppuccin = {
+      enable = true;
+      settings.transparent_background.__raw = "vim.g.neovide or false";
+    };
     extraPackages = with pkgs; [
       ripgrep
       fd
@@ -20,6 +23,31 @@
         vim.keymap.set("", "q", "<Nop>", {})
         vim.keymap.set("", "<leader>Q", "q", {desc = "Record Macro"})
       '';
+    # Neovide config
+    extraConfigLua = ''
+      if vim.g.neovide then
+        local font = "JetBrainsMono Nerd Font"
+
+        local font_size = vim.o.lines < 60 and 11 or 12
+
+        vim.o.guifont = font .. ":h" .. font_size
+        vim.g.neovide_transparency = 0.7
+        vim.g.transparency = 0.8
+        vim.g.neovide_window_blurred = true
+
+        vim.keymap.set("n", "<c-->", function()
+            font_size = font_size - 1
+            vim.o.guifont = font .. ":h" .. font_size
+            vim.notify("Font Set: " .. font .. ":h" .. font_size)
+        end, { desc = "Decrease font size" })
+
+        vim.keymap.set("n", "<c-=>", function()
+            font_size = font_size + 1
+            vim.o.guifont = font .. ":h" .. font_size
+            vim.notify("Font Set: " .. font .. ":h" .. font_size)
+        end, { desc = "Increase font size" })
+      end
+    '';
     keymaps = import ./keymaps.nix;
     globals = {
       # Set leader key to space.
