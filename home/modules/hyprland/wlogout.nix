0ba3d9840b2f89b27c3110c578ae14b78aae1343
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2025-02-26 18:00:03 +0700

    refactor(terminal): migrate to catppuccin theme system
    BREAKING CHANGE: Removes stylix theming in favor of catppuccin
    
    - Replace wlogout's custom styling with catppuccin theme
    - Remove ghostty custom theme in favor of catppuccin
    - Add kitty keybindings and remove stylix integration
    - Standardize terminal font configurations

--- home/modules/hyprland/wlogout.nix
+++ home/modules/hyprland/wlogout.nix
@@ -4,97 +4,33 @@ let
 in
 {
   config = lib.mkIf cfg.enable {
-    home.file.".config/wlogout/icons" = {
-      source = ./wlogout-icons;
-      recursive = true;
-    };
+    catppuccin.wlogout.extraStyle = # css
+      ''
+        #lock {
+          background-image: image(url("${./wlogout-icons/lock.png}"));
+        }
+
+        #logout {
+          background-image: image(url("${./wlogout-icons/logout.png}"));
+        }
+
+        #suspend {
+          background-image: image(url("${./wlogout-icons/suspend.png}"));
+        }
+
+        #hibernate {
+          background-image: image(url("${./wlogout-icons/hibernate.png}"));
+        }
+
+        #shutdown {
+          background-image: image(url("${./wlogout-icons/shutdown.png}"));
+        }
+
+        #reboot {
+          background-image: image(url("${./wlogout-icons/reboot.png}"));
+        }
+      '';
     programs.wlogout = {
-      # style =
-      #   # css
-      #   ''
-      #     /* -----------------------------------------------------
-      #      * Import Pywal colors
-      #      * ----------------------------------------------------- */
-      #     @import "${config.home.homeDirectory}/.cache/wallust/wlogout.css";
-      #
-      #     /* -----------------------------------------------------
-      #      * General
-      #      * ----------------------------------------------------- */
-      #
-      #     * {
-      #       font-family: "Fira Sans Semibold", FontAwesome, Roboto, Helvetica, Arial, sans-serif;
-      #       background-image: none;
-      #       transition: 20ms;
-      #       box-shadow: none;
-      #     }
-      #
-      #     button {
-      #       color: #ffffff;
-      #       font-size: 20px;
-      #
-      #       background-repeat: no-repeat;
-      #       background-position: center;
-      #       background-size: 25%;
-      #
-      #       border-style: solid;
-      #       background-color: rgba(12, 12, 12, 0.3);
-      #       border: 3px solid #ffffff;
-      #
-      #       box-shadow:
-      #         0 4px 8px 0 rgba(0, 0, 0, 0.2),
-      #         0 6px 20px 0 rgba(0, 0, 0, 0.19);
-      #     }
-      #
-      #     button:focus,
-      #     button:active,
-      #     button:hover {
-      #       color: @color11;
-      #       background-color: rgba(12, 12, 12, 0.5);
-      #       border: 3px solid @color11;
-      #     }
-      #
-      #     /*
-      #     -----------------------------------------------------
-      #     Buttons
-      #     -----------------------------------------------------
-      #     */
-      #
-      #     #lock {
-      #       margin: 10px;
-      #       border-radius: 20px;
-      #       background-image: image(url("icons/lock.png"));
-      #     }
-      #
-      #     #logout {
-      #       margin: 10px;
-      #       border-radius: 20px;
-      #       background-image: image(url("icons/logout.png"));
-      #     }
-      #
-      #     #suspend {
-      #       margin: 10px;
-      #       border-radius: 20px;
-      #       background-image: image(url("icons/suspend.png"));
-      #     }
-      #
-      #     #hibernate {
-      #       margin: 10px;
-      #       border-radius: 20px;
-      #       background-image: image(url("icons/hibernate.png"));
-      #     }
-      #
-      #     #shutdown {
-      #       margin: 10px;
-      #       border-radius: 20px;
-      #       background-image: image(url("icons/shutdown.png"));
-      #     }
-      #
-      #     #reboot {
-      #       margin: 10px;
-      #       border-radius: 20px;
-      #       background-image: image(url("icons/reboot.png"));
-      #     }
-      #   '';
       enable = true;
       layout = [
         {
@@ -135,36 +71,6 @@ in
         }
       ];
     };
-    home.file.".config/wallust/templates/wlogout.css".text = # css
-      ''
-        @define-color foreground {{foreground}};
-        @define-color background {{background}};
-        @define-color cursor {{cursor}};
-
-        @define-color color0 {{color0}};
-        @define-color color1 {{color1}};
-        @define-color color2 {{color2}};
-        @define-color color3 {{color3}};
-        @define-color color4 {{color4}};
-        @define-color color5 {{color5}};
-        @define-color color6 {{color6}};
-        @define-color color7 {{color7}};
-        @define-color color8 {{color8}};
-        @define-color color9 {{color9}};
-        @define-color color10 {{color10}};
-        @define-color color11 {{color11}};
-        @define-color color12 {{color12}};
-        @define-color color13 {{color13}};
-        @define-color color14 {{color14}};
-        @define-color color15 {{color15}};
-
-
-        window {
-            background: url("${config.home.homeDirectory}/.cache/wallpaper/blurred.png");
-            background-size: cover;
-        }
-      '';
-
     profile.hyprland.wallust.settings.templates.wlogout =
       let
         out = config.home.homeDirectory + "/.cache/wallust";
--- home/programs/kitty.nix
+++ home/programs/kitty.nix
@@ -1,8 +1,4 @@
-{
-  config,
-  lib,
-  ...
-}:
+{ config, lib, ... }:
 let
   cfg = config.profile.kitty;
 in
@@ -31,7 +27,21 @@ in
         background_blur = 40;
         cursor_blink_interval = toString 0.5;
       };
+      keybindings = {
+        "ctrl+a>enter" = "launch --location=vsplit --cwd=current";
+        "ctrl+a>backspace" = "launch --location=hsplit --cwd=current";
+        "ctrl+a>h" = "neighboring_window left";
+        "ctrl+a>j" = "neighboring_window down";
+        "ctrl+a>k" = "neighboring_window up";
+        "ctrl+a>l" = "neighboring_window right";
+        "ctrl+a>t" = "new_tab_with_cwd";
+        "ctrl+a>w" = "close_window";
+        "ctrl+a>r" = "start_resizing_window";
+      };
+      font = {
+        name = "JetBrainsMono Nerd Font Mono";
+        size = 11;
+      };
     };
-    stylix.targets.kitty.enable = true;
   };
 }
