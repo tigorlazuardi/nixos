310a72f3fc79f0064ba1d9129739382d6cc466d3
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2025-02-28 11:16:04 +0700

    hyprcursor: move back to rose-pine-hyprcursor

--- home/modules/hyprland/hyprcursor.nix
+++ home/modules/hyprland/hyprcursor.nix
@@ -33,50 +33,14 @@ let
       maintainers = with maintainers; [ ndom91 ];
     };
   });
-  future-cyan-hyprcursor = pkgs.stdenv.mkDerivation (finalAttrs: rec {
-    pname = "future-cyan-hyprcursor";
-    version = "60fc69d603a6d7b99c1841a2c4cebd130b1aa357";
-    src = pkgs.fetchFromGitLab {
-      owner = "Pummelfisch";
-      repo = "future-cyan-hyprcursor";
-      rev = version;
-      sha256 = "sha256-TRDSQFCwofNj3PbGdE4Ro1hyQV7nJuE2Gc7YSUvv4k0=";
-    };
-    installPhase = ''
-      runHook preInstall
-
-      mkdir -p $out/share/icons
-      cp -R "./Future-Cyan-Hyprcursor_Theme" "$out/share/icons"
-
-      runHook postInstall
-    '';
-  });
-  nordzy-hyprcursors-white = pkgs.stdenv.mkDerivation (finalAttrs: rec {
-    pname = "nordzy-hyprcursors-white";
-    version = "2.3.0";
-    src = pkgs.fetchzip {
-      url = "https://github.com/guillaumeboehm/Nordzy-cursors/releases/download/v${version}/Nordzy-hyprcursors-white.tar.gz";
-      sha256 = "sha256-lTqMh5xukMn5LgFz5toVfSwuXsPDERcpnUGuzH8af8k=";
-    };
-    installPhase = ''
-      runHook preInstall
-
-      mkdir -p $out/share/icons
-      cp -R . $out/share/icons
-
-      runHook postInstall
-    '';
-  });
 in
 {
   config = lib.mkIf cfg.enable {
     home.packages = [
       rose-pine-hyprcursor
-      future-cyan-hyprcursor
-      nordzy-hyprcursors-white
     ];
     home.sessionVariables = {
-      HYPRCURSOR_THEME = "Future-Cyan-Hyprcursor_Theme";
+      HYPRCURSOR_THEME = "rose-pine-hyprcursor";
       HYPRCURSOR_SIZE = "28";
     };
   };
