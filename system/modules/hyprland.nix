a6f525037b3c50b0a5a65c558b4e6352e9df761c
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2025-03-10 11:16:52 +0700

    regreet: disable_hyprland_qtutils_check

--- system/modules/hyprland.nix
+++ system/modules/hyprland.nix
@@ -111,7 +111,6 @@ in
                 misc {
                     disable_hyprland_logo = true
                     disable_splash_rendering = true
-                    disable_hyprland_qtutils_check = true
                 }
               '';
         in
