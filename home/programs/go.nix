d6a11223eb9a8eb9fa0752d94ff6f03c83c5f7ab
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2025-03-12 17:03:54 +0700

    go: exposed binaries for vscode to pickup

--- home/programs/go.nix
+++ home/programs/go.nix
@@ -36,7 +36,38 @@ in
       impl
       golangci-lint
       wgo
+      delve
     ];
+
+    home.file."go/bin" =
+      let
+        goPackages = with pkgs; [
+          go
+          wgo
+          gotools
+          gomodifytags
+          gotests
+          iferr
+          gopls
+          gofumpt
+          impl
+          golangci-lint
+          delve
+        ];
+        merged = pkgs.symlinkJoin {
+          name = "home-go-bin";
+          paths = goPackages;
+        };
+      in
+      {
+        source = "${merged}/bin";
+        recursive = true;
+      };
+
+    home.file."go/bin/gopls".source = "${pkgs.gopls}/bin/gopls";
+    home.file."go/bin/dlv".source = "${pkgs.delve}/bin/dlv";
+    home.file."go/bin/dlv-dap".source = "${pkgs.delve}/bin/dlv-dap";
+    home.file."go/bin/impl".source = "${pkgs.impl}/bin/impl";
 
     # Some toolings will lookup for $GOROOT env.
     home.sessionVariables = {
