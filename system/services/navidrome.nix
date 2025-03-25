b5c2d682923603d30053020c1b2595c11cbcb156
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2025-03-25 21:23:39 +0700

    navidrome: added music.tigor.web.id as extra domain

--- system/services/navidrome.nix
+++ system/services/navidrome.nix
@@ -3,25 +3,37 @@ let
   cfg = config.profile.services.navidrome;
   user = config.profile.user;
   domain = "navidrome.tigor.web.id";
+  extraDomain = "music.tigor.web.id";
   inherit (lib) mkIf;
 in
 {
   config = mkIf cfg.enable {
-    services.nginx.virtualHosts."${domain}" = {
-      useACMEHost = "tigor.web.id";
-      forceSSL = true;
-      locations = {
-        "/" = {
-          proxyPass = "http://0.0.0.0:${toString config.services.navidrome.settings.Port}";
-          proxyWebsockets = true;
+    services.nginx.virtualHosts =
+      let
+        opts = {
+          useACMEHost = "tigor.web.id";
+          forceSSL = true;
+          locations = {
+            "/" = {
+              proxyPass = "http://0.0.0.0:${toString config.services.navidrome.settings.Port}";
+              proxyWebsockets = true;
+            };
+          };
         };
+      in
+      {
+        "${domain}" = opts;
+        "${extraDomain}" = opts;
       };
-    };
 
-    security.acme.certs."tigor.web.id".extraDomainNames = [ domain ];
+    security.acme.certs."tigor.web.id".extraDomainNames = [
+      domain
+      extraDomain
+    ];
 
     services.adguardhome.settings.user_rules = [
       "192.168.100.5 ${domain}"
+      "192.168.100.5 ${extraDomain}"
     ];
 
     users.groups.navidrome.members = [ user.name ];
