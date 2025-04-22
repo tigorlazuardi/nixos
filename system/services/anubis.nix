42912dea8785cb31ea2941f83f2276c3cb4a6779
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2025-04-22 23:32:43 +0700

    anubis: by default serve robots.txt

--- system/services/anubis.nix
+++ system/services/anubis.nix
@@ -13,5 +13,9 @@ in
     users.users.nginx.extraGroups = [
       config.users.groups.anubis.name
     ];
+
+    services.anubis.defaultOptions.settings = {
+      SERVE_ROBOTS_TXT = true;
+    };
   };
 }
