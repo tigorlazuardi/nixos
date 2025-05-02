f50c07b3d1c367aabfaf33aaf9a00e721c91d35b
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2025-05-02 15:49:32 +0700

    spotify: removed azurmeau fork of spotifywm

--- home/programs/spotify.nix
+++ home/programs/spotify.nix
@@ -9,17 +9,17 @@ let
 in
 {
   config = lib.mkIf cfg.enable {
-    home.packages = with pkgs; [
-      (spotifywm.overrideAttrs {
-        version = "azurmeau-fork";
-        src = fetchFromGitHub {
-          owner = "amurzeau";
-          repo = "spotifywm";
-          rev = "b2222c9da47b278a3addef48250513420df405ac";
-          hash = "sha256-kRBOV2jPJ81xGEgCbSBcOo4Ie9FoK1kfxXMQG1vhHfM=";
-        };
-      })
-    ];
+    # home.packages = with pkgs; [
+    #   (spotifywm.overrideAttrs {
+    #     version = "azurmeau-fork";
+    #     src = fetchFromGitHub {
+    #       owner = "amurzeau";
+    #       repo = "spotifywm";
+    #       rev = "b2222c9da47b278a3addef48250513420df405ac";
+    #       hash = "sha256-kRBOV2jPJ81xGEgCbSBcOo4Ie9FoK1kfxXMQG1vhHfM=";
+    #     };
+    #   })
+    # ];
 
     services.swaync.settings.scripts._10-spotify = {
       app-name = "spotifywm";
