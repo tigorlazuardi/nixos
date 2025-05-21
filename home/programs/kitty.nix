541197462fd874fd7dc492ad97700a2fde628ed8
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2025-05-21 14:05:29 +0700

    kitty: add keybinding to select tab

--- home/programs/kitty.nix
+++ home/programs/kitty.nix
@@ -38,17 +38,26 @@ in
         copy_on_select = true;
         strip_trailing_spaces = "smart";
       };
-      keybindings = {
-        "ctrl+a>enter" = "launch --location=vsplit --cwd=current";
-        "ctrl+a>backspace" = "launch --location=hsplit --cwd=current";
-        "ctrl+a>h" = "neighboring_window left";
-        "ctrl+a>j" = "neighboring_window down";
-        "ctrl+a>k" = "neighboring_window up";
-        "ctrl+a>l" = "neighboring_window right";
-        "ctrl+a>t" = "new_tab_with_cwd";
-        "ctrl+a>w" = "close_window";
-        "ctrl+a>r" = "start_resizing_window";
-      };
+      keybindings =
+        {
+          "ctrl+a>enter" = "launch --location=vsplit --cwd=current";
+          "ctrl+a>backspace" = "launch --location=hsplit --cwd=current";
+          "ctrl+a>h" = "neighboring_window left";
+          "ctrl+a>j" = "neighboring_window down";
+          "ctrl+a>k" = "neighboring_window up";
+          "ctrl+a>l" = "neighboring_window right";
+          "ctrl+a>t" = "new_tab_with_cwd";
+          "ctrl+a>w" = "close_window";
+          "ctrl+a>n" = "new_window_with_cwd";
+          "ctrl+a>r" = "start_resizing_window";
+          "ctrl+a>0" = "select_tab";
+        }
+        // builtins.listToAttrs (
+          map (i: {
+            name = "ctrl+a>${i}";
+            value = "goto_tab ${i}";
+          }) (map toString (lib.lists.range 1 9))
+        );
       # font = {
       #   name = "JetBrainsMono Nerd Font Mono";
       #   size = 11;
