9b14fb77b445485a9ccf3df4c3b80ec6173b3bac
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2025-05-31 19:41:08 +0700

    syncthing: simplified configurations and added anubis instance to the web interface

--- system/services/syncthing.nix
+++ system/services/syncthing.nix
@@ -20,11 +20,13 @@ in
       forceSSL = true;
       locations = {
         "/" = {
-          proxyPass = "http://0.0.0.0:8384";
+          proxyPass = "http://unix:${config.services.anubis.instances.syncthing.settings.BIND}";
           proxyWebsockets = true;
         };
       };
     };
+
+    services.anubis.instances.syncthing.settings.TARGET = "http://0.0.0.0:8384";
 
     sops.secrets =
       let
@@ -53,115 +55,29 @@ in
             id = "Redmage/Laptop-Kerja";
             devices = [ "work-laptop" ];
           };
-          "/nas/redmage/images/s20fe-sfw" = {
-            label = "Redmage/S20FE";
-            id = "Redmage/S20FE";
-            devices = [ "s20fe" ];
-          };
           "/nas/redmage/images/oppo-find-x8" = {
             label = "Redmage/oppo-find-x8";
             id = "Redmage/oppo-find-x8";
             devices = [ "oppo-find-x8" ];
-          };
-          "/nas/Syncthing/Sync/Japanese-Homework" = {
-            label = "Japanese Homework";
-            id = "Japanese-Homework";
-            devices = [
-              "s20fe"
-              "onyx"
-              "windows"
-            ];
-          };
-          "/nas/Syncthing/Sync/PCSX2" = {
-            label = "PCSX2";
-            id = "PCSX2";
-            devices = [
-              "windows"
-              "steam-deck"
-              "oppo-find-x8"
-              "living-room-system"
-            ];
           };
           "/nas/Syncthing/Sync/Windows" = {
             label = "Windows";
             id = "Windows";
             devices = [ "windows" ];
           };
-          "/nas/Syncthing/Sync/Rips" = {
-            label = "Rips";
-            id = "Rips";
-            devices = [ "windows" ];
-          };
-          "/nas/kavita/library/light-novels" = {
-            label = "Light Novels";
-            id = "Light-Novels";
-            devices = [ "onyx" ];
-          };
-          "/nas/Syncthing/Sync/VPN" = {
-            label = "OpenVPN";
-            id = "OpenVPN";
-            devices = [
-              "s20fe"
-              "work-laptop"
-            ];
-          };
           "/nas/Syncthing/Sync/WireGuard" = {
             label = "WireGuard";
             id = "WireGuard";
             # devices = lib.attrsets.mapAttrsToList (key: _value: key) config.services.syncthing.settings.devices;
             devices = [
-              "s20fe"
               "work-laptop"
               "oppo-find-x8"
-            ];
-          };
-          "/nas/photos/mama" = {
-            label = "Camera Mama";
-            id = "sm-s906e_8dch-photos";
-            devices = [ "samsung-s22-mama" ];
-          };
-          "/nas/photos/tigor" = {
-            label = "Camera Tigor";
-            id = "sm-g780f_yjwa-photos";
-            devices = [
-              "s20fe"
-            ];
-          };
-          "/nas/Syncthing/Sync/Onyx-Notes" = {
-            label = "Onyx Notes";
-            id = "Onyx-Notes";
-            devices = [ "onyx" ];
-          };
-          "/nas/Syncthing/Sync/Japanese-Learning-Materials" = {
-            label = "Japanese Learning Materials";
-            id = "Japanese-Learning-Materials";
-            devices = [
-              "s20fe"
-              "work-laptop"
-            ];
-          };
-          "/nas/Syncthing/Sync/Memes" = {
-            label = "Memes";
-            id = "Memes";
-            devices = [
-              "s20fe"
-              "work-laptop"
-            ];
-          };
-          "/nas/EmuDeck" = {
-            label = "EmuDeck";
-            id = "EmuDeck";
-            devices = [
-              "steam-deck"
-              "windows"
-              "living-room-system"
             ];
           };
           "/nas/Syncthing/Sync/sops" = {
             label = "Sops";
             id = "Sops";
             devices = [
-              "s20fe"
               "work-laptop"
               "windows"
               "oppo-find-x8"
@@ -171,7 +87,6 @@ in
             label = "Music";
             id = "Music";
             devices = [
-              "s20fe"
               "work-laptop"
               "windows"
               "living-room-system"
@@ -182,7 +97,6 @@ in
             label = "General";
             id = "General";
             devices = [
-              "s20fe"
               "work-laptop"
               "windows"
               "living-room-system"
@@ -193,7 +107,6 @@ in
             label = "Public";
             id = "Public";
             devices = [
-              "s20fe"
               "work-laptop"
               "windows"
               "living-room-system"
@@ -202,14 +115,6 @@ in
           };
         };
         devices = {
-          s20fe = {
-            name = "Samsung S20FE";
-            id = "ASH4PGY-H2ANIMX-RJJRODR-AD6KH5X-632CAG2-5NCDSGN-I27XNAC-EMVL6A7";
-          };
-          onyx = {
-            name = "Onyx Note Air 3";
-            id = "FZMFBD5-5PS566H-XJGV3FO-NQVSMX5-3VHPS7V-SUT27WA-MXHFBYT-BDSS6AW";
-          };
           windows = {
             name = "Windows";
             id = "FSTIYS6-REFXIJX-KPLYC4L-QSZO46L-RV3VTPZ-VWVTE7O-Y663OZN-RTKP3QI";
@@ -217,14 +122,6 @@ in
           work-laptop = {
             name = "Work Laptop";
             id = "BOU76IK-5AE7ARF-ZQDFOTX-KWUQL22-SAGXBYG-B75JRZA-L4MCYPU-OYTY5AU";
-          };
-          samsung-s22-mama = {
-            name = "Samsung S22 Mama";
-            id = "5G2Q7XE-HILUI46-GWTE6P6-NJHAG3A-HSZKMAU-K5PBOKR-QN3IFQO-GX7KTQU";
-          };
-          steam-deck = {
-            name = "Steam Deck";
-            id = "6SOR4SU-MVT2XIS-4J6IGVP-LITFLDB-ZH6LA7T-PUSQK26-P6RVWZ7-YB7P4AX";
           };
           living-room-system = {
             name = "Living Room System";
