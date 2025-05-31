bd5e373142194991c9579bbc4e3e82defce3e0d9
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2025-05-31 21:54:53 +0700

    memos: added anubis integration and user/group setup

--- system/podman/memos.nix
+++ system/podman/memos.nix
@@ -5,43 +5,99 @@ let
   inherit (lib) mkIf;
   ip = "10.88.88.1";
   image = "docker.io/neosmemo/memos:stable";
-  rootVolume = "/nas/podman/memos";
+  rootVolume = "/wolf/podman/memos";
   domain = "${name}.tigor.web.id";
   user = config.profile.user;
-  uid = toString user.uid;
-  gid = toString user.gid;
 in
 {
   config = mkIf (podman.enable && podman.${name}.enable) {
+
+    users = {
+      groups.${name}.gid = 971;
+      users = {
+        ${name} = {
+          uid = 976;
+          isSystemUser = true;
+          description = "Unprivileged Podman container user for ${name}";
+          group = name;
+        };
+        ${user.name}.extraGroups = [
+          name
+        ];
+      };
+    };
+
     services.nginx.virtualHosts.${domain} = {
       useACMEHost = "tigor.web.id";
       forceSSL = true;
-      locations."/".proxyPass = "http://${ip}:5230";
+      locations."/".proxyPass = "http://unix:${config.services.anubis.instances.memos.settings.BIND}";
     };
 
-    system.activationScripts."podman-${name}" = ''
-      mkdir -p ${rootVolume}
-      chown ${uid}:${gid} ${rootVolume}
-    '';
+    system.activationScripts."podman-${name}" =
+      let
+        uid = toString config.users.users.${name}.uid;
+        gid = toString config.users.groups.${name}.gid;
+      in
+      ''
+        mkdir -p ${rootVolume}
+        chown -R ${uid}:${gid} ${rootVolume}
+      '';
 
-    virtualisation.oci-containers.containers.${name} = {
-      inherit image;
-      hostname = name;
-      autoStart = true;
-      user = "${uid}:${gid}";
-      environment = {
-        TZ = "Asia/Jakarta";
-        # MEMOS_PUBLIC = "true";
-      };
-      volumes = [ "${rootVolume}:/var/opt/memos" ];
-      extraOptions = [
-        "--network=podman"
-        "--ip=${ip}"
+    services.anubis.instances.memos = {
+      settings.TARGET = "http://${ip}:5230";
+      botPolicy = [
+        {
+          name = "allow-apis";
+          path_regex = ''^/memos\.api.*$'';
+          action = "ALLOW";
+        }
+        {
+          name = "well-known";
+          path_regex = ''^/.well-known/.*$'';
+          action = "ALLOW";
+        }
+        {
+          name = "favicon";
+          path_regex = ''^/favicon.ico$'';
+          action = "ALLOW";
+        }
+        {
+          name = "robots-txt";
+          path_regex = ''^/robots.txt$'';
+          action = "ALLOW";
+        }
+        {
+          name = "catch-all-challenge-browsers";
+          path_regex = "Mozilla";
+          action = "CHALLENGE";
+        }
       ];
-      labels = {
-        "io.containers.autoupdate" = "registry";
-      };
     };
+
+    virtualisation.oci-containers.containers.${name} =
+      let
+        uid = toString config.users.users.${name}.uid;
+        gid = toString config.users.groups.${name}.gid;
+      in
+      {
+        inherit image;
+        hostname = name;
+        autoStart = true;
+        user = "${uid}:${gid}";
+        environment = {
+          TZ = "Asia/Jakarta";
+          # MEMOS_PUBLIC = "true";
+        };
+        volumes = [ "${rootVolume}:/var/opt/memos" ];
+        extraOptions = [
+          "--network=podman"
+          "--ip=${ip}"
+          "--umask=0007"
+        ];
+        labels = {
+          "io.containers.autoupdate" = "registry";
+        };
+      };
   };
 
 }
