49311e2e89611f6c691aa43f86813f5bb7d715dc
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2025-05-31 16:43:49 +0700

    ytptube: changed user and group to a service account and changed jellyfin user to be in the ytptube group. Also added umask to the container options.

--- system/podman/ytptube.nix
+++ system/podman/ytptube.nix
@@ -13,75 +13,94 @@ let
   volume = "/wolf/mediaserver/${name}";
   domain = "${name}.tigor.web.id";
   user = config.profile.user;
-  uid = toString user.uid;
-  gid = toString user.gid;
-  webhook = builtins.readFile (
-    (pkgs.formats.json { }).generate "webhooks.json" [
-      {
-        name = "NTFY Webhook Added";
-        on = [ "added" ];
-        request = {
-          url = "https://ntfy.tigor.web.id/ytptube?click=https%3A%2F%2F${domain}&tpl=1&t=%7B%7B.title%7D%7D&m=%5B%7B%7B%20.folder%20%7D%7D%5D%20Download%20added.";
-          type = "json";
-          method = "POST";
-          headers = {
-            Authorization = ''Bearer ${config.sops.placeholder."ntfy/tokens/homeserver"}'';
-            X-Tags = "rocket";
-          };
-        };
-      }
-      {
-        name = "NTFY Webhook Completed";
-        on = [ "completed" ];
-        request = {
-          url = "https://ntfy.tigor.web.id/ytptube?click=https%3A%2F%2F${domain}&tpl=1&t=%7B%7B.title%7D%7D&m=%5B%7B%7B%20.folder%20%7D%7D%5D%20Download%20%7B%7B%20.status%20%7D%7D";
-          type = "json";
-          method = "POST";
-          headers = {
-            Authorization = ''Bearer ${config.sops.placeholder."ntfy/tokens/homeserver"}'';
-            X-Tags = "heavy_check_mark";
-            X-Priority = "4";
-          };
-        };
-      }
-      {
-        name = "NTFY Webhook Error";
-        on = [ "error" ];
-        request = {
-          url = "https://ntfy.tigor.web.id/ytptube?click=https%3A%2F%2F${domain}&tpl=1&t=%7B%7B.title%7D%7D&m=%5B%7B%7B%20.folder%20%7D%7D%5D%20Download%20%7B%7B%20.status%20%7D%7D";
-          type = "json";
-          method = "POST";
-          headers = {
-            Authorization = ''Bearer ${config.sops.placeholder."ntfy/tokens/homeserver"}'';
-            X-Priority = "4";
-            X-Tags = "x";
-          };
-        };
-      }
-    ]
-  );
+  # webhook = builtins.readFile (
+  #   (pkgs.formats.json { }).generate "webhooks.json" [
+  #     {
+  #       name = "NTFY Webhook Added";
+  #       on = [ "added" ];
+  #       request = {
+  #         url = "https://ntfy.tigor.web.id/ytptube?click=https%3A%2F%2F${domain}&tpl=1&t=%7B%7B.title%7D%7D&m=%5B%7B%7B%20.folder%20%7D%7D%5D%20Download%20added.";
+  #         type = "json";
+  #         method = "POST";
+  #         headers = {
+  #           Authorization = ''Bearer ${config.sops.placeholder."ntfy/tokens/homeserver"}'';
+  #           X-Tags = "rocket";
+  #         };
+  #       };
+  #     }
+  #     {
+  #       name = "NTFY Webhook Completed";
+  #       on = [ "completed" ];
+  #       request = {
+  #         url = "https://ntfy.tigor.web.id/ytptube?click=https%3A%2F%2F${domain}&tpl=1&t=%7B%7B.title%7D%7D&m=%5B%7B%7B%20.folder%20%7D%7D%5D%20Download%20%7B%7B%20.status%20%7D%7D";
+  #         type = "json";
+  #         method = "POST";
+  #         headers = {
+  #           Authorization = ''Bearer ${config.sops.placeholder."ntfy/tokens/homeserver"}'';
+  #           X-Tags = "heavy_check_mark";
+  #           X-Priority = "4";
+  #         };
+  #       };
+  #     }
+  #     {
+  #       name = "NTFY Webhook Error";
+  #       on = [ "error" ];
+  #       request = {
+  #         url = "https://ntfy.tigor.web.id/ytptube?click=https%3A%2F%2F${domain}&tpl=1&t=%7B%7B.title%7D%7D&m=%5B%7B%7B%20.folder%20%7D%7D%5D%20Download%20%7B%7B%20.status%20%7D%7D";
+  #         type = "json";
+  #         method = "POST";
+  #         headers = {
+  #           Authorization = ''Bearer ${config.sops.placeholder."ntfy/tokens/homeserver"}'';
+  #           X-Priority = "4";
+  #           X-Tags = "x";
+  #         };
+  #       };
+  #     }
+  #   ]
+  # );
 in
-lib.mkMerge [
-  (mkIf podman.${name}.enable {
+{
+  config = mkIf podman.${name}.enable {
+    users = {
+      groups.${name} = { };
+      users = {
+        ${user.name}.extraGroups = [ name ];
+        ${name} = {
+          isSystemUser = true;
+          description = "Unprivileged system account for ${name} service";
+          group = name;
+        };
+        jellyfin = lib.mkIf config.services.jellyfin.enable {
+          extraGroups = [ name ];
+        };
+      };
+
+    };
+
     services.nginx.virtualHosts.${domain} = {
       useACMEHost = "tigor.web.id";
       forceSSL = true;
       locations."/" = {
         proxyPass = "http://${ip}:8081";
         proxyWebsockets = true;
-        # extraConfig = # nginx
-        #   ''
-        #     auth_basic $auth_ip;
-        #   '';
+        extraConfig = # nginx
+          ''
+            auth_basic $auth_ip;
+          '';
       };
     };
 
-    system.activationScripts."podman-${name}" = ''
-      mkdir -p ${volume}
-      chown -R ${uid}:${gid} ${volume} /etc/podman/${name}
-    '';
+    system.activationScripts."podman-${name}" =
+      let
+        uid = toString config.users.users.${name}.uid;
+        gid = toString config.users.groups.${name}.gid;
+      in
+      ''
+        mkdir -p ${volume}
+        chown -R ${uid}:${gid} ${volume} /etc/podman/${name} 
+      '';
 
-    systemd.services."podman-${name}".restartTriggers = [ webhook ];
+    # systemd.services."podman-${name}".restartTriggers = [ webhook ];
 
     environment.etc."podman/${name}/ytdlp.json" = {
       # https://github.com/arabcoders/ytptube?tab=readme-ov-file#ytdlpjson-file
@@ -122,41 +141,46 @@ lib.mkMerge [
       mode = "0444";
     };
 
-    virtualisation.oci-containers.containers.${name} = {
-      inherit image;
-      hostname = name;
-      autoStart = true;
-      user = "${uid}:${gid}";
-      environment = {
-        TZ = "Asia/Jakarta";
-        YTP_MAX_WORKERS = "4";
-      };
-      volumes = [
-        "${volume}:/downloads"
-        "/etc/podman/${name}:/config"
-      ];
-      extraOptions = [
-        "--ip=${ip}"
-        "--network=podman"
-      ];
-      labels = {
-        "io.containers.autoupdate" = "registry";
-      };
-    };
-  })
-  {
-    profile.services.ntfy-sh.client.settings.subscribe =
+    virtualisation.oci-containers.containers.${name} =
       let
-        vueIcon = pkgs.fetchurl {
-          url = "https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/120px-Vue.js_Logo_2.svg.png";
-          hash = "sha256-chQ5dFB22qfGpGnYJ9B6NsUWlbfAeIIoJL5bSyz2YEg=";
-        };
+        uid = toString config.users.users.${name}.uid;
+        gid = toString config.users.groups.${name}.gid;
       in
-      [
-        {
-          command = ''${pkgs.libnotify}/bin/notify-send --app-name="ytptube" --icon="${vueIcon}" --category=im.received --urgency=normal "$title" "$message"'';
-          topic = "ytptube";
-        }
-      ];
-  }
-]
+      {
+        inherit image;
+        hostname = name;
+        autoStart = true;
+        user = "${uid}:${gid}";
+        environment = {
+          TZ = "Asia/Jakarta";
+          YTP_MAX_WORKERS = "4";
+        };
+        volumes = [
+          "${volume}:/downloads"
+          "/etc/podman/${name}:/config"
+        ];
+        extraOptions = [
+          "--ip=${ip}"
+          "--network=podman"
+          "--umask=0002"
+        ];
+        labels = {
+          "io.containers.autoupdate" = "registry";
+        };
+      };
+
+    # profile.services.ntfy-sh.client.settings.subscribe =
+    #   let
+    #     vueIcon = pkgs.fetchurl {
+    #       url = "https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/120px-Vue.js_Logo_2.svg.png";
+    #       hash = "sha256-chQ5dFB22qfGpGnYJ9B6NsUWlbfAeIIoJL5bSyz2YEg=";
+    #     };
+    #   in
+    #   [
+    #     {
+    #       command = ''${pkgs.libnotify}/bin/notify-send --app-name="ytptube" --icon="${vueIcon}" --category=im.received --urgency=normal "$title" "$message"'';
+    #       topic = "ytptube";
+    #     }
+    #   ];
+  };
+}
