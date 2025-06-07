0a107dbddbfcf2d5f42d4631bbef9085a7402f04
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2025-06-07 14:34:22 +0700

    anubis: removed default bot policy because it cannot resolve (data) syntax

--- system/services/anubis.nix
+++ system/services/anubis.nix
@@ -19,37 +19,12 @@ in
     environment.systemPackages = lib.mkIf hasInstances [
       (pkgs.writeShellScriptBin "restart-anubis" ''
         set -e
-        units=$(systemctl list-units --output json | jq -r '.[] | select(.unit | startswith("anubis-")) | .unit')
+        units=$(systemctl list-units --output json | ${pkgs.jq}/bin/jq -r '.[] | select(.unit | startswith("anubis-")) | .unit')
         for unit in $units; do
           echo "Restarting $unit"
           systemctl restart "$unit"
         done
       '')
     ];
-
-    services.anubis.defaultOptions =
-      let
-        defaultBotPolicy = with builtins; fromJSON (readFile "${inputs.anubis}/data/botPolicies.json");
-        botPolicy = defaultBotPolicy // {
-          bots = [
-            {
-              name = "punish-empty-user-agent";
-              expression = ''userAgent == ""'';
-              action = "CHALLENGE";
-              challenge = {
-                difficulty = 16;
-                report_as = 4;
-                algorithm = "slow";
-              };
-            }
-
-          ] ++ defaultBotPolicy.bots;
-        };
-      in
-      {
-        inherit botPolicy;
-        settings.POLICY_FNAME = (pkgs.formats.yaml { }).generate "botPolicy.yaml" botPolicy;
-      };
-
   };
 }
