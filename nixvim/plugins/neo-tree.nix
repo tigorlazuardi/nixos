3ce6b4ec0e95758d4a9374f0852efe39d78deb42
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2025-06-11 13:02:11 +0700

    nixvim(neo-tree): add extra options for filesystem filtering and key mappings

--- nixvim/plugins/neo-tree.nix
+++ nixvim/plugins/neo-tree.nix
@@ -2,13 +2,44 @@
   programs.nixvim = {
     plugins.neo-tree = {
       enable = true;
+      extraOptions = {
+        filesystem = {
+          filtered_items = {
+            visible = true;
+            never_show_by_pattern = [
+              ".aider*"
+            ];
+          };
+          never_show = [
+            ".DS_Store"
+            ".git"
+            ".hg"
+            ".svn"
+            "thumbs.db"
+          ];
+        };
+        window.mappings = {
+          "<space>" = false;
+          "]b" = "next_source";
+          "[b" = "prev_source";
+          o = "open";
+          l = "open";
+          h = "close_node";
+        };
+      };
     };
     keymaps = [
       {
-        action = "<cmd>Neotree toggle<cr>";
+        action = "<cmd>Neotree toggle reveal focus<cr>";
         key = "<leader>e";
         mode = "n";
         options.desc = "Toggle Neo-tree";
+      }
+      {
+        action = "<cmd>Neotree toggle buffers focus<cr>";
+        key = "<leader>bb";
+        mode = "n";
+        options.desc = "Neo-tree buffers";
       }
     ];
   };
