874fbb28f2bb54d0258997cffe977fb0064458f2
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2025-06-14 14:02:19 +0700

    morphos: now uses authelia

--- system/podman/morphos.nix
+++ system/podman/morphos.nix
@@ -1,4 +1,9 @@
-{ config, lib, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 let
   name = "morphos";
   podman = config.profile.podman;
@@ -6,17 +11,17 @@ let
   ip = "10.88.88.2";
   image = "ghcr.io/danvergara/morphos-server:latest";
   domain = "${name}.tigor.web.id";
-  user = config.profile.user;
-  uid = toString user.uid;
-  gid = toString user.gid;
+  socketAddress = "/run/podman/${name}.sock";
 in
 {
   config = mkIf (podman.enable && podman.${name}.enable) {
     services.nginx.virtualHosts.${domain} = {
       useACMEHost = "tigor.web.id";
       forceSSL = true;
+      enableAuthelia = true;
+      autheliaLocations = [ "/" ];
       locations."/" = {
-        proxyPass = "http://unix:${config.services.anubis.instances.${name}.settings.BIND}";
+        proxyPass = "http://unix:${socketAddress}";
         extraConfig = # nginx
           ''
             client_max_body_size 100M;
@@ -24,13 +29,36 @@ in
       };
     };
 
-    services.anubis.instances.${name}.settings.TARGET = "http://${ip}:8080";
+    systemd.services."podman-${name}" = {
+      unitConfig.StopWhenUnneeded = true;
+      serviceConfig.ExecStartPost = "${pkgs.waitport}/bin/waitport ${ip} 8080";
+    };
+
+    systemd.sockets."podman-${name}-proxy" = {
+      listenStreams = [ socketAddress ];
+      wantedBy = [ "sockets.target" ];
+    };
+
+    systemd.services."podman-${name}-proxy" = {
+      unitConfig = {
+        Requires = [
+          "podman-${name}.service"
+          "podman-${name}-proxy.socket"
+        ];
+        After = [
+          "podman-${name}.service"
+          "podman-${name}-proxy.socket"
+        ];
+      };
+      serviceConfig = {
+        ExecStart = "${pkgs.systemd}/lib/systemd/systemd-socket-proxyd --exit-idle-time=15m ${ip}:8080";
+      };
+    };
 
     virtualisation.oci-containers.containers.${name} = {
       inherit image;
       hostname = name;
-      autoStart = true;
-      user = "${uid}:${gid}";
+      autoStart = false;
       environment = {
         TZ = "Asia/Jakarta";
       };
