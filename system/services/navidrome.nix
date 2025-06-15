2c090575239f6d35d06799148266af36e4523427
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2025-06-15 17:44:09 +0700

    navidrome: add socket activation support and anubis protection

--- system/services/navidrome.nix
+++ system/services/navidrome.nix
@@ -1,10 +1,16 @@
-{ config, lib, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 let
   cfg = config.profile.services.navidrome;
   user = config.profile.user;
   domain = "navidrome.tigor.web.id";
   extraDomain = "music.tigor.web.id";
   inherit (lib) mkIf;
+  socketAddress = "/run/navidrome.sock";
 in
 {
   config = mkIf cfg.enable {
@@ -14,8 +20,10 @@ in
           useACMEHost = "tigor.web.id";
           forceSSL = true;
           locations = {
+            "/api".proxyPass = "http://unix:${socketAddress}"; # bypass API endpoints from anubis
+            "/rest".proxyPass = "http://unix:${socketAddress}"; # bypass REST API endpoints from anubis
             "/" = {
-              proxyPass = "http://0.0.0.0:${toString config.services.navidrome.settings.Port}";
+              proxyPass = "http://unix:${config.services.anubis.instances.navidrome.settings.BIND}";
               proxyWebsockets = true;
             };
           };
@@ -29,10 +37,47 @@ in
     users.groups.navidrome.members = [ user.name ];
     users.groups.${user.name}.members = [ "navidrome" ];
 
+    services.anubis.instances.navidrome.settings.TARGET = "unix://${socketAddress}";
+
+    systemd.services.navidrome = {
+      unitConfig.StopWhenUnneeded = true;
+      serviceConfig.ExecStartPost =
+        let
+          settings = config.services.navidrome.settings;
+        in
+        [ "${pkgs.waitport}/bin/waitport ${settings.Address} ${toString settings.Port}" ];
+      wantedBy = lib.mkForce [ ]; # This service will be started by systemd-socket-activation
+    };
+
+    systemd.sockets.navidrome-proxy = {
+      listenStreams = [ socketAddress ];
+      wantedBy = [ "sockets.target" ];
+    };
+
+    systemd.services.navidrome-proxy = {
+      unitConfig = {
+        Requires = [
+          "navidrome.service"
+          "navidrome-proxy.socket"
+        ];
+        After = [
+          "navidrome.service"
+          "navidrome-proxy.socket"
+        ];
+      };
+      serviceConfig =
+        let
+          settings = config.services.navidrome.settings;
+        in
+        {
+          ExecStart = "${pkgs.systemd}/lib/systemd/systemd-socket-proxyd --exit-idle-time=5m ${settings.Address}:${toString settings.Port}";
+        };
+    };
+
     services.navidrome = {
       enable = true;
       settings = {
-        Address = "0.0.0.0";
+        Address = "127.0.0.1";
         MusicFolder = "/nas/Syncthing/Sync/Music";
       };
     };
