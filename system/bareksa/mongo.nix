5ea2bc5cd9e5dcb714dff67a90cf6d386fed98ab
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2025-06-17 11:48:56 +0700

    bareksa: prepare for mongo

--- system/bareksa/mongo.nix
+++ system/bareksa/mongo.nix
@@ -5,15 +5,6 @@
   ...
 }:
 let
-  user = config.profile.user;
-  uid = toString user.uid;
-  gid = toString user.gid;
-  mkTemplate = uri: {
-    owner = user.name;
-    content = ''
-      MONGO_URL=${uri}
-    '';
-  };
   instances = [
     {
       ip = "10.88.245.1";
@@ -40,81 +31,6 @@ let
 in
 {
   config = lib.mkIf config.profile.environment.bareksa.enable {
-    sops = {
-      secrets = builtins.listToAttrs (
-        map (instance: {
-          name = instance.secretName;
-          value = {
-            sopsFile = ../../secrets/bareksa.yaml;
-            owner = user.name;
-          };
-        }) instances
-      );
-      templates = builtins.listToAttrs (
-        map (instance: {
-          name = instance.secretName;
-          value = mkTemplate config.sops.placeholder."${instance.secretName}";
-        }) instances
-      );
-    };
-
-    virtualisation.oci-containers.containers = builtins.listToAttrs (
-      map (instance: {
-        name = instance.name;
-        value = {
-          image = "docker.io/quickbooks2018/mongo-gui:latest";
-          autoStart = false;
-          user = "${uid}:${gid}";
-          extraOptions = [
-            "--network=podman"
-            "--ip=${instance.ip}"
-          ];
-          environment = {
-            TZ = "Asia/Jakarta";
-          };
-          environmentFiles = [ config.sops.templates."${instance.secretName}".path ];
-          labels = {
-            "io.containers.autoupdate" = "registry";
-          };
-        };
-      }) instances
-    );
-
-    systemd.sockets = builtins.listToAttrs (
-      map (instance: {
-        name = "podman-${instance.name}-proxy";
-        value = {
-          listenStreams = [ instance.socketAddress ];
-          wantedBy = [ "sockets.target" ];
-        };
-      }) instances
-    );
-
-    systemd.services = builtins.listToAttrs (
-      (map (instance: {
-        name = "podman-${instance.name}";
-        value = {
-          unitConfig.StopWhenUnneeded = true;
-        };
-      }) instances)
-      ++ (map (instance: {
-        name = "podman-${instance.name}-proxy";
-        value = {
-          unitConfig = {
-            Requires = [
-              "podman-${instance.name}.service"
-              "podman-${instance.name}-proxy.socket"
-            ];
-            After = [
-              "podman-${instance.name}.service"
-              "podman-${instance.name}-proxy.socket"
-            ];
-          };
-          serviceConfig.ExecStart = ''${pkgs.systemd}/lib/systemd/systemd-socket-proxyd --exit-idle-time=15m ${instance.ip}:4321'';
-        };
-      }) instances)
-    );
-
     services.nginx.virtualHosts = builtins.listToAttrs (
       map (instance: {
         name = instance.domain;
