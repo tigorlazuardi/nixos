9d7a8d8cd285356bfc523e6349accba753ba699a
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2025-07-07 23:51:37 +0700

    ssh: update homeserver SSH keys

--- hardware-configuration/homeserver.nix
+++ hardware-configuration/homeserver.nix
@@ -156,10 +156,10 @@ in
     };
 
     users.users."${config.profile.user.name}".openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB1X6NS0rzXAt31RTKBQKH0Evo8NH7qJPyNEAefzc1Yw tigor@castle"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEExzLJott/tOrK02fXgaQwp/5Fd+sOsDt+g0foWCf7D termux@oppo-find-x8"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDWAaeNAJ/AY9X6W0bmcVcdB2rSt0AnzmKyyBqhrl5Nj tigor@windows"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOg+zjg+YqBA0OfLatp5okqytRxZPEeykeNE6hWXB4NT tigor@for"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIPQzsIv7DPww62CbhdGddTLTErsJzpfowxRIYBR1P+9 tigor@castle"
     ];
 
     services.nginx.virtualHosts."public.tigor.web.id" = {
